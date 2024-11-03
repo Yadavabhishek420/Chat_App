36f8da8087883e02b88bb2966c21a8f831aea3a3
Abhishek  Yadav <kabhishek3096@gmail.com>
2024-11-03 12:53:20 +0530

    this is folder added

---
+++ lib/Shared/Constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:748127160548:web:e589323bc2f4bc076c84f1";
+  static String apiKey = "AIzaSyCmXSomsedD_9-guSqI7aNEUgzyasgZNSE";
+  static String messagingSenderId = "748127160548";
+  static String projectId = "flutter-8e51a";
+  final primaryColor = const Color(0xFFee7b64);
+}
